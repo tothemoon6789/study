34be034d50299c92a0c72a48f069bd4e9ed905f6
tothemoon6789 <pnlinh6789@gmail.com>
2024-02-19 11:06:43 +0700

    st

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-buttons = document.querySelectorAll('button');
-buttons.forEach(e => {
-    e.onclick = function(){
-        var demo = document.getElementById('demo');
-        demo.className = e.innerText;
-    }
+buttons = document.querySelectorAll("button");
+buttons.forEach((e) => {
+  e.addEventListener("click", function () {
+    var demo = document.getElementById("demo");
+    demo.innerText = "Nhà báo – Nhà thơ Phạm Quốc Cường sinh năm 1980 tại Nam Đàn – Nghệ An, bút danh: Quốc Đô, Quốc Minh, Trà My, TQC, QM, HM. Phạm Quốc Cường hiện là Trưởng ban Pháp luật – Bạn đọc, thuộc Báo Pháp luật Việt Nam.Các tập thơ đã xuất bản: Viết cho người tình mơ; Anh chờ qua trăng; Tình hoa; Tình yêu còn lại; Khúc ca tình đời; Tuyển chọn 100 bài thơ tình và đời; Bước đời. Phạm Quốc Cường cũng là tác giả của nhiều ca khúc được yêu thíchCác giải thưởng: Tham gia chỉ đạo, biên tập, tổ chức xuất bản loạt bài của nhóm phóng viên Báo điện tử Pháp luật Việt Nam (Phapluatplus.vn) được trao giải C giải Báo chí Quốc gia năm 2016; Giải thưởng báo chí toàn quốc phòng, chống tham nhũng, tiêu cực các năm 2020-2021, 2022-2023; Giải thưởng báo chí toàn quốc về Quốc hội và Hội đồng nhân dân (Giải Diên Hồng, tháng 1 năm 2024); Các giải thưởng Báo chí Bộ, ngành, địa phương; Bằng khen, giấy khen của các Bộ, ngành Trung ương, và nhiều giải thưởng khác về văn hóa, nghệ thuật, thể thao.";
+    demo.className = e.innerText;
+    demo.scrollIntoView();
+  });
 });
+function flexGrow(e) {
+  e.addEventListener("click", function () {
+    var demo = document.getElementById("demo");
+    var html = `<div>mot</div><div>hai</div><div>ba</div><div>bon</div>`;
+    demo.innerHTML = html;
+  });
+}
