6429bfe03c9e6d61dca30fa11cf8b42d5d45256e
tothemoon6789 <pnlinh6789@gmail.com>
2024-03-05 08:28:49 +0700

    st

--- main.js
+++ main.js
@@ -95,7 +95,35 @@ document.getElementById("float").addEventListener("click", function () {
   }
 })();
 (function () {
-  var id = ["kichthuoc", "vitri", "mausac"];
+  "use strict";
+  var c = [
+    { id: "transition", note: "", warning: "" },
+  ];
+  c.forEach(function (e) {
+    document.getElementById(e.id).addEventListener("click", function () {
+      flex(e);
+    });
+  });
+  function flex(e) {
+    clear();
+    var html = "Nhà báo – Nhà thơ Phạm Quốc Cường sinh năm 1980 tại Nam Đàn – Nghệ An, bút danh: Quốc Đô, Quốc Minh, Trà My, TQC, QM, HM. Phạm Quốc Cường hiện là Trưởng ban Pháp luật – Bạn đọc, thuộc Báo Pháp luật Việt Nam.Các tập thơ đã xuất bản: Viết cho người tình mơ; Anh chờ qua trăng; Tình hoa; Tình yêu còn lại; Khúc ca tình đời; Tuyển chọn 100 bài thơ tình và đời; Bước đời. Phạm Quốc Cường cũng là tác giả của nhiều ca khúc được yêu thíchCác giải thưởng: Tham gia chỉ đạo, biên tập, tổ chức xuất bản loạt bài của nhóm phóng viên Báo điện tử Pháp luật Việt Nam (Phapluatplus.vn) được trao giải C giải Báo chí Quốc gia năm 2016; Giải thưởng báo chí toàn quốc phòng, chống tham nhũng, tiêu cực các năm 2020-2021, 2022-2023; Giải thưởng báo chí toàn quốc về Quốc hội và Hội đồng nhân dân (Giải Diên Hồng, tháng 1 năm 2024); Các giải thưởng Báo chí Bộ, ngành, địa phương; Bằng khen, giấy khen của các Bộ, ngành Trung ương, và nhiều giải thưởng khác về văn hóa, nghệ thuật, thể thao.";
+    var demo = document.getElementById("demo");
+    demo.innerHTML = html;
+    e.note !== ""
+      ? (function () {
+          document.getElementById("note").innerHTML = e.note;
+          document.getElementById("note").classList.add("show");
+        })()
+      : (function () {})();
+    e.wraning !== ""
+      ? (function () {
+          document.getElementById("warning").innerHTML = e.warning;
+        })()
+      : (function () {})();
+  }
+})();
+(function () {
+  var id = ["kichthuoc", "vitri", "mausac","chuyendong"];
   var control = [];
   id.forEach((e) => {
     control.push("control" + e);
@@ -117,3 +145,8 @@ function clear() {
   document.getElementById("note").innerText = "";
   document.getElementById("warning").innerText = "";
 }
+(function () {
+  "use strit";
+  var total = document.querySelectorAll(".container button");
+  document.getElementById("total").innerText = total.length;
+})();
